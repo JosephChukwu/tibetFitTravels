39c3e85366cafc082bd76d15c829965d96337e0b
Warriorr <chukwuj59@gmail.com>
2024-08-02 11:45:48 +0100

    finished tthe form

--- src/components/Backdrop.jsx
+++ src/components/Backdrop.jsx
@@ -27,3 +27,179 @@ const Backdrop = ({ children, onClick }) => {
 };
 
 export default Backdrop;
+
+
+
+
+
+
+// import { React, useState } from "react";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import * as Yup from "yup";
+// import { FiArrowRight } from "react-icons/fi";
+// import Swal from "sweetalert2";
+// import "sweetalert2/dist/sweetalert2.min.css";
+// // import HashLoader from "react-spinners/ClipLoader";
+// import FullScreenLoader from "./FullScreenLoader";
+
+// const scriptUrl =
+//   "https://script.google.com/macros/s/AKfycbwZnShRQ4vhYtmmtuM7SR34vy4-Im-juOYI3Z1sZbSKYpSA8wXPxfj8OuesYJ2QBXtzwQ/exec";
+
+// const validationSchema = Yup.object({
+//   Fullname: Yup.string().required("Name is required!"),
+//   WhatsAppnumber: Yup.string().required("WhatsApp number is Required!"),
+//   Email: Yup.string()
+//     .email("Invalid email address")
+//     .required("Email is required!"),
+// });
+
+// const MyForm = () => {
+//     const [loading, setloading] = useState(false)
+//   const initialValues = {
+//     Fullname: "",
+//     WhatsAppnumber: "",
+//     Email: "",
+//   };
+
+//   const handleSubmit = async (
+//     values,
+//     { setSubmitting, resetForm, isSubmitting }
+//   ) => {
+//     //setsubmitting to true to trigger the spinner
+//     setSubmitting(true);
+
+//     try {
+//       const response = await fetch(scriptUrl, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(values),
+//         //removed cors cuz google api was blocking the request based o coos pilicy
+//         mode: "no-cors",
+//       });
+
+//       //small delay before poppingup the sweetalert
+//       await new Promise((resolve) => setTimeout(resolve, 250));
+//       console.log("Form submitted:", values);
+
+//       //Via sweetalert
+//       Swal.fire({
+//         title: "Successfully Joined Waitlist!",
+//         text: "Click the WhatsApp Button to join our Waitlist Group",
+//         icon: "success",
+//         showCancelButton: true,
+//         confirmButtonText: "WhatsApp",
+//         cancelButtonText: "Cancel",
+//       }).then((result) => {
+//         if (result.isConfirmed) {
+//           // now direct the user to the whatspp link
+//           window.location.href =
+//             "https://chat.whatsapp.com/F9ZEVNWjqHeEA49ZYCwNRi";
+//         }
+//       });
+//       // toast.success("Thanks for joining our Waitlist!");
+//     } catch (error) {
+//       console.log(error.message);
+//       Swal.fire({
+//         icon: "error",
+//         title: "Oops...",
+//         text: "There was an error loading the page. Please check your internet connection.",
+//       });
+//       setSubmitting(false);
+
+//       // toast.error('Please check you internet and try again!')
+//     }
+
+//     resetForm();
+//   };
+
+//   return (
+//     <div className="contact-form-container">
+//       <FullScreenLoader loading={loading} />
+
+//       <Formik
+//         initialValues={initialValues}
+//         validationSchema={validationSchema}
+//         onSubmit={handleSubmit}
+//       >
+//         {({ isSubmitting }) => (
+//           <Form style={{ width: "300px" }}>
+//             <div style={{ marginBottom: "10px" }}>
+//               <label htmlFor="Fullname" style={{ color: "white" }}>
+//                 Your Name
+//               </label>
+//               <Field
+//                 type="text"
+//                 id="Fullname"
+//                 name="Fullname"
+//                 className="formField"
+//               />
+//               <ErrorMessage
+//                 name="Fullname"
+//                 component="div"
+//                 style={{ color: "red" }}
+//               />
+//             </div>
+
+//             <div style={{ marginBottom: "10px" }}>
+//               <label htmlFor="WhatsAppnumber" style={{ color: "white" }}>
+//                 WhatsApp Number
+//               </label>
+//               <Field
+//                 type="text"
+//                 id="WhatsAppnumber"
+//                 name="WhatsAppnumber"
+//                 className="formField"
+//               />
+//               <ErrorMessage
+//                 name="WhatsAppnumber"
+//                 component="div"
+//                 style={{ color: "red" }}
+//               />
+//             </div>
+
+//             <div style={{ marginBottom: "10px" }}>
+//               <label htmlFor="email" style={{ color: "white" }}>
+//                 Email
+//               </label>
+//               <Field
+//                 className="formField"
+//                 type="text"
+//                 id="Email"
+//                 name="Email"
+//               />
+//               <ErrorMessage
+//                 name="Email"
+//                 component="div"
+//                 style={{ color: "red" }}
+//               />
+//             </div>
+
+//             <button
+//               className="secondary-button"
+//               type="submit"
+//               style={{
+//                 width: "100%",
+//                 borderRadius: "27px",
+//                 padding: "10px",
+//                 outline: "none",
+//               }}
+//             >
+//               {isSubmitting ? (
+//                 // <BallTriangle color="#fffff" height={100} width={100} />
+//                 <FullScreenLoader loading={isSubmitting} />
+//               ) : (
+//                 <div>
+//                   Join WaitList <FiArrowRight />
+//                 </div>
+//               )}
+//             </button>
+//           </Form>
+//         )}
+//       </Formik>
+//     </div>
+//   );
+// };
+
+// export default MyForm;
--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,11 +1,17 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { motion } from "framer-motion";
 import Backdrop from './Backdrop'; // Assuming this is your custom backdrop component
-import { Grid, TextField, Button } from '@mui/material';
+import { Grid, Button, Typography, TextField } from '@mui/material';
 import { styled } from "@mui/system";
-import { useFormik } from 'formik';
+import { Formik, Form, Field } from "formik";
 import * as Yup from 'yup';
-import emailjs from 'emailjs-com';
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import MoonLoader  from "react-spinners/ClipLoader";
+
+
+
+
 
 const MotionGrid = motion(
   styled(Grid)({})
@@ -33,30 +39,63 @@ const dropIn = {
 }
 
 const ContactModal = ({ handleClose }) => {
-  const form = useRef(); // Create a ref for the form
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbz3xQVB1kvMMukOwIMJYueITYgVFcDJJXsSkg0drgBfNDBbpv6mj5vVK445ShMdsw0w/exec";
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      location: '',
-      message: '',
-    },
-    validationSchema: Yup.object({
-      name: Yup.string().required('Required'),
-      email: Yup.string().email('Invalid email address').required('Required'),
-      location: Yup.string().required('Required'),
-      message: Yup.string().required('Required'),
-    }),
-    onSubmit: () => {
-      emailjs.sendForm('service_hp98szb', 'template_92tojzs', form.current, 'f15kThW9_D-ndvVpO')
-        .then((result) => {
-          alert('Message sent successfully!');
-        }, (error) => {
-          alert('Failed to send message. Please try again later.');
-        });
-    },
+  const initialValues = {
+    Name: '',
+    Email: '',
+    Nationality: '',
+    Subject: '',
+    Message: ''
+  };
+
+  const validationSchema = Yup.object({
+    Name: Yup.string().required('Required'),
+    Email: Yup.string().email('Invalid email address').required('Required'),
+    Nationality: Yup.string().required('Required'),
+    Subject: Yup.string().required('Required'),
+    Message: Yup.string().required('Required')
   });
+
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    setSubmitting(true);
+    try {
+     const response = await fetch(scriptUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },       
+
+        body: JSON.stringify(values),
+        mode: "no-cors",
+      });
+      // console.log('sent req')
+
+
+      await new Promise((resolve) => setTimeout(resolve, 250));
+      // console.log("Form submitted:", values);
+
+      Swal.fire({
+        title: "Enquiry sent!",
+        text: "We will provide you with a response shortly.",
+        icon: "success",
+        confirmButtonText: "Close",
+        willClose: () => {
+          handleClose(); 
+        }
+      });
+    } catch (error) {
+      console.error(error.message);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "There was an error. Please try again.",
+      });
+    } finally {
+      setSubmitting(false);
+      resetForm();
+    }
+  };
 
   return (
     <Backdrop onClick={handleClose}>
@@ -79,76 +118,332 @@ const ContactModal = ({ handleClose }) => {
         animate="visible"
         exit="exit"
       >
-        <form ref={form} onSubmit={formik.handleSubmit}>
-          <TextField
-            label="Name"
-            name="name"
-            multiline
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            error={formik.touched.name && Boolean(formik.errors.name)}
-            helperText={formik.touched.name && formik.errors.name}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Email"
-            name="email"
-            type="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            error={formik.touched.email && Boolean(formik.errors.email)}
-            helperText={formik.touched.email && formik.errors.email}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Location"
-            name="location"
-            value={formik.values.location}
-            onChange={formik.handleChange}
-            error={formik.touched.location && Boolean(formik.errors.location)}
-            helperText={formik.touched.location && formik.errors.location}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Message"
-            name="message"
-            multiline
-            rows={4}
-            value={formik.values.message}
-            onChange={formik.handleChange}
-            error={formik.touched.message && Boolean(formik.errors.message)}
-            helperText={formik.touched.message && formik.errors.message}
-            fullWidth
-            margin="normal"
-            sx={{
-              // '& .MuiInputBase-input': {
-              //   color: 'white', // Change text color
-              //   '::placeholder': {
-              //     color: 'grey', // Change placeholder color
-              //   },
-              // },
-              // '& .MuiInputLabel-root': {
-              //   color: 'white', // Change label color
-              // },
-              // '& .MuiInput-underline:after': {
-              //   borderBottomColor: 'white', // Change underline color
-              // },
-            }}
-          />
-          <Button type="submit" variant="contained" color="primary">
-            Submit
-          </Button>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ values, handleChange, touched, errors, isSubmitting }) => (
+            <Form style={{ width: "90%" }}>
+              <Field
+                as={TextField}
+                name="Name"
+                label="Name"
+                fullWidth
+                margin="normal"
+                value={values.Name}
+                onChange={handleChange}
+                error={touched.Name && Boolean(errors.Name)}
+                helperText={touched.Name && errors.Name}
+              />
+              <Field
+                as={TextField}
+                name="Email"
+                label="Email"
+                type="email"
+                fullWidth
+                margin="normal"
+                value={values.Email}
+                onChange={handleChange}
+                error={touched.Email && Boolean(errors.Email)}
+                helperText={touched.Email && errors.Email}
+              />
+              <Field
+                as={TextField}
+                name="Nationality"
+                label="Nationality"
+                fullWidth
+                margin="normal"
+                value={values.Nationality}
+                onChange={handleChange}
+                error={touched.Nationality && Boolean(errors.Nationality)}
+                helperText={touched.Nationality && errors.Nationality}
+              />
+              <Field
+                as={TextField}
+                name="Subject"
+                label="Subject"
+                fullWidth
+                margin="normal"
+                value={values.Subject}
+                onChange={handleChange}
+                error={touched.Subject && Boolean(errors.Subject)}
+                helperText={touched.Subject && errors.Subject}
+              />
+              <Field
+                as={TextField}
+                name="Message"
+                label="Message"
+                multiline
+                rows={4}
+                fullWidth
+                margin="normal"
+                value={values.Message}
+                onChange={handleChange}
+                error={touched.Message && Boolean(errors.Message)}
+                helperText={touched.Message && errors.Message}
+              />
+              <Button type="submit" variant="contained" color="primary" disabled={isSubmitting}>
+                {isSubmitting ? <MoonLoader color='#0379a9'/> : <Typography variant='body1'>Send enquiry</Typography>}
+              </Button>
+            </Form>
+          )}
+        </Formik>
       </MotionGrid>
     </Backdrop>
-  )
+  );
 }
 
 export default ContactModal;
 
 
-// google sheets link
-// https://docs.google.com/spreadsheets/d/1Wh01878HSAKjnW2cnyB2zyuwbHxIzbiM49UpMF_l8SY/edit?usp=sharing
+
+
+
+
+
+// import React, { useRef } from 'react';
+// import { motion } from "framer-motion";
+// import Backdrop from './Backdrop'; // Assuming this is your custom backdrop component
+// import { Grid, TextField, Button } from '@mui/material';
+// import { styled } from "@mui/system";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import * as Yup from 'yup';
+// import Swal from "sweetalert2";
+// import "sweetalert2/dist/sweetalert2.min.css";
+
+
+// const MotionGrid = motion(
+//   styled(Grid)({})
+// );
+
+// const dropIn = {
+//   hidden: {
+//     y: "-100vh",
+//     opacity: 0
+//   },
+//   visible: {
+//     y: "0",
+//     opacity: 1,
+//     transition: {
+//       duration: 0.1,
+//       type: "spring",
+//       damping: 25,
+//       stiffness: 500
+//     }
+//   },
+//   exit: {
+//     y: "100vh",
+//     opacity: 0
+//   }
+// }
+
+// const ContactModal = ({ handleClose }) => {
+
+//   const scriptUrl =
+  // "https://docs.google.com/spreadsheets/d/1Wh01878HSAKjnW2cnyB2zyuwbHxIzbiM49UpMF_l8SY/edit?resourcekey=&gid=2103818021#gid=2103818021";
+
+//    const initialValues = {
+//       Name: '',
+//       Email: '',
+//       Nationality: '',
+//       Subject: '',
+//       Message: ''
+//     }
+//    const validationSchema = Yup.object({
+//       Name: Yup.string().required('Required'),
+//       Email: Yup.string().email('Invalid email address').required('Required'),
+//       Nationality: Yup.string().required('Required'),
+//       Message: Yup.string().required('Required'),
+//     })
+//     const handleSubmit = async (
+//       values,
+//       { setSubmitting, resetForm, isSubmitting }
+//     ) => {
+//       //setsubmitting to true to trigger the spinner
+//       setSubmitting(true);
+  
+//       try {
+//         const response = await fetch(scriptUrl, {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(values),
+//           //removed cors cuz google api was blocking the request based o coos pilicy
+//           mode: "no-cors",
+//         });
+  
+//         //small delay before poppingup the sweetalert
+//         await new Promise((resolve) => setTimeout(resolve, 250));
+//         console.log("Form submitted:", values);
+  
+//         //Via sweetalert
+//         Swal.fire({
+//           title: "Successfully Joined Waitlist!",
+//           text: "Click the WhatsApp Button to join our Waitlist Group",
+//           icon: "success",
+//           showCancelButton: true,
+//           confirmButtonText: "WhatsApp",
+//           cancelButtonText: "Cancel",
+//         }).then((result) => {
+//           if (result.isConfirmed) {
+//             // now direct the user to the whatspp link
+//             window.location.href =
+//               "https://chat.whatsapp.com/F9ZEVNWjqHeEA49ZYCwNRi";
+//           }
+//         });
+//         // toast.success("Thanks for joining our Waitlist!");
+//       } catch (error) {
+//         console.log(error.message);
+//         Swal.fire({
+//           icon: "error",
+//           title: "Oops...",
+//           text: "There was an error loading the page. Please check your internet connection.",
+//         });
+//         setSubmitting(false);
+  
+//         // toast.error('Please check you internet and try again!')
+//       }
+  
+//       resetForm();
+//     };
+
+
+  
+
+//   return (
+//     <Backdrop onClick={handleClose}>
+//       <MotionGrid
+//         onClick={(e) => e.stopPropagation()}
+//         sx={{
+//           width: "70%",
+//           height: "auto",
+//           padding: "0.5rem",
+//           margin: "auto",
+//           borderRadius: "12px",
+//           display: "flex",
+//           flexDirection: "column",
+//           alignItems: "center",
+//           color: "white",
+//           backgroundColor: "#f4f4f4"
+//         }}
+//         variants={dropIn}
+//         initial="hidden"
+//         animate="visible"
+//         exit="exit"
+//       >
+
+//       <Formik
+//         initialValues={initialValues}
+//         validationSchema={validationSchema}
+//         onSubmit={handleSubmit}
+//       >
+//       {({ isSubmitting }) => (
+
+//       <Form style={{ width: "300px" }}>
+//       <TextField
+//             label="Name"
+//             name="name"
+//             multiline
+//             value={formik.values.name}
+//             onChange={formik.handleChange}
+//             error={formik.touched.name && Boolean(formik.errors.name)}
+//             helperText={formik.touched.name && formik.errors.name}
+//             fullWidth
+//             margin="normal"
+//           />
+//           <TextField
+//             label="Email"
+//             name="email"
+//             type="email"
+//             value={formik.values.email}
+//             onChange={formik.handleChange}
+//             error={formik.touched.email && Boolean(formik.errors.email)}
+//             helperText={formik.touched.email && formik.errors.email}
+//             fullWidth
+//             margin="normal"
+//           />
+//           <TextField
+//             label="Location"
+//             name="location"
+//             value={formik.values.location}
+//             onChange={formik.handleChange}
+//             error={formik.touched.location && Boolean(formik.errors.location)}
+//             helperText={formik.touched.location && formik.errors.location}
+//             fullWidth
+//             margin="normal"
+//           />
+//           <TextField
+//             label="Message"
+//             name="message"
+//             multiline
+//             rows={4}
+//             value={formik.values.message}
+//             onChange={formik.handleChange}
+//             error={formik.touched.message && Boolean(formik.errors.message)}
+//             helperText={formik.touched.message && formik.errors.message}
+//             fullWidth
+//             margin="normal"
+//             sx={{
+             
+//             }}
+//           />
+//           <Button type="submit" variant="contained" color="primary">
+//           {isSubmitting ? (
+//             // <BallTriangle color="#fffff" height={100} width={100} />
+//               <Typography>Loading</Typography>
+//           ) : (
+//             <Typography>send request</Typography>
+
+//           )}      
+//               </Button>
+//           </Form>
+//         )}
+
+//           </Formik>
+//       </MotionGrid>
+//     </Backdrop>
+//   )
+// }
+
+// export default ContactModal;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Replace 'your_spreadsheet_url' with your actual spreadsheet URL
+// const sheets = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1Wh01878HSAKjnW2cnyB2zyuwbHxIzbiM49UpMF_l8SY/edit?resourcekey=&gid=2103818021#gid=2103818021");
+// const sheet = sheets.getSheetByName("Sheet1");
+
+// function doPost(e) {
+//   // Add CORS headers
+//   var headers = {
+//     'Content-Type': 'application/json',
+//     'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Methods': 'POST',
+//     'Access-Control-Allow-Headers': 'Content-Type',
+//   };
+
+//   try {
+//     let data = JSON.parse(e.postData.contents);
+//     sheet.appendRow([data.Name, data.Email, data.Nationality, data.Subject, data.Message]);
+//     return ContentService.createTextOutput(
+//       JSON.stringify({ success: true, message: "Request Submitted!" })
+//     ).setMimeType(ContentService.MimeType.JSON).setHeaders(headers);
+//   } catch (error) {
+//     return ContentService.createTextOutput(
+//       JSON.stringify({ success: false, message: "Error adding data" })
+//     ).setMimeType(ContentService.MimeType.JSON).setHeaders(headers);
+//   }
+// }
